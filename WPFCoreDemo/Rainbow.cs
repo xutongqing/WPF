dbc537e3959252d873735b5c96e88f79d98b985a
xutongqing <xutongqing@qianxin.com>
2020-03-16 07:04:37 +0800

    *

---
+++ WPFCoreDemo/Rainbow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFCoreDemo
+{
+    public enum Rainbow
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet
+    }
+}
